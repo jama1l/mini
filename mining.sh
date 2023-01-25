04d198a6d219350ae0465f04aabf78a48833ce65
jama1l <123479915+jama1l@users.noreply.github.com>
2023-01-25 03:21:05 -0800

    Create mining.sh

---
+++ mining.sh
@@ -0,0 +1,1 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u R9Zv8d3c8xtv3WBT2w7dYdjTff6zwBkStn.quk1 -p x --cpu 2
